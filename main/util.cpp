3d0dbb46167a49ff82e83e4bc290264f6b6c6e7e
Muhammad Shahroz <shahrozjavid5373@gmail.com>
2024-06-01 14:25:12 +0500

    Update util.cpp

--- main/util.cpp
+++ main/util.cpp
@@ -3,15 +3,10 @@ using namespace std;
 #include "util.h"
 
 void addNode(const int& data, Node*& current) {
-	if (current == NULL) {
-		current = new Node(data);
-		current = current->next;
-	}
-	else {
 		current->next = new Node(data);
 		current->next->previous = current;
 		current = current->next;
-	}
+	
 }
 
 void printList(Node* current) {
